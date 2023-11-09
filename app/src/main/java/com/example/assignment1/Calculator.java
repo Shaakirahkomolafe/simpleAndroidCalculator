93353a4053ac5c3939c90caae1ddaa4dca48ca75
Shakirat komolafe <damolakomolafe.dk@gmail.com>
2023-11-09 18:26:21 -0500

    add logic for the calculator with arithmetic operations and history

--- app/src/main/java/com/example/assignment1/Calculator.java
+++ app/src/main/java/com/example/assignment1/Calculator.java
@@ -1,4 +1,92 @@
 package com.example.assignment1;
 
+
 public class Calculator {
-}
+    String currentInput = "";
+    char operator;
+
+    boolean numError, calculationDone, operatorAbsent, operatorPresent, numberFound;
+    int secondNumber = 0 , firstNumber = 0, result = 0;
+    public void push(String value) {
+
+        currentInput += value;
+    }
+
+
+    int calculate() {
+        char[] chars = currentInput.toCharArray();
+        numError = false;
+        calculationDone = false;
+        operatorAbsent = false;
+        operatorPresent = false;
+        numberFound = false;
+
+        for (int i = 0; i < chars.length && !numError; i+= 2) {
+            if (!Character.isDigit(chars[i]) || Character.isDigit(chars[i + 1])) {
+                numError = true;
+            }
+        }
+
+
+        for (int i = 0; i < chars.length && !numError; i++) {
+            switch (chars[i]) {
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    operatorPresent = true;
+                    operator = chars[i];
+                case '=':
+                    if (i != 1) calculationDone = true;
+                    break;
+                default:
+                    break;
+
+            }
+            if (operatorPresent && i > 0) {
+                if (!numberFound) {
+                    if (Character.isDigit(chars[i - 1]))
+                        firstNumber = Integer.parseInt(String.valueOf(chars[i - 1]));
+                    else
+                        numError = true;
+                }
+                    if (Character.isDigit(chars[i+1]))
+                        secondNumber = Integer.parseInt(String.valueOf(chars[i+1]));
+                    else
+                        numError = true;
+
+                    if (!numError) {
+                        switch (operator) {
+                            case '+':
+                                result = firstNumber + secondNumber;
+                                break;
+                            case '-':
+                                result = firstNumber - secondNumber;
+                                break;
+                            case '*':
+                                result = firstNumber * secondNumber;
+                                break;
+                            case '/':
+                                result = firstNumber / secondNumber;
+                                break;
+                            default:
+                                operatorAbsent = true;
+                                break;
+
+                        }
+
+                        firstNumber = result;
+
+                        numberFound= true;
+                        operatorPresent = false;
+                    }
+                }
+            }
+            if (numError || operatorAbsent)
+                return -1;
+            else
+                return result;
+
+        }
+
+    }
--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -9,13 +9,22 @@ import android.widget.Button;
 
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView displayText;
+
+    TextView historyText;
     Button button0,button1, button2, button3, button4, button5, button6, button7, button8,button9;
     Button buttonAdd, buttonMinus, buttonMultiply, buttonDivide, buttonEnter, buttonCancel, buttonHistory;
 
+    boolean showHistory = false;
+
+    boolean nextNumber = false;
+    Calculator calculatorObject;
+    ArrayList<String> historyList;
 
 
 
@@ -25,7 +34,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        calculatorObject = new Calculator();
+
       displayText = (TextView) findViewById(R.id.result);
+      displayText.setText("");
+      historyText = (TextView) findViewById(R.id.history);
+      historyText.setText("");
         button0 = findViewById(R.id.button0);
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
@@ -44,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonCancel = findViewById(R.id.cancel);
         buttonHistory = findViewById(R.id.advance);
 
+
+        button0.setOnClickListener(this);
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
         button3.setOnClickListener (this);
@@ -61,12 +77,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonCancel.setOnClickListener(this);
         buttonHistory.setOnClickListener(this);
 
-
+            historyList = new ArrayList<>();
 
     }
+
+    void clearResult(){
+        calculatorObject.currentInput = "";
+        displayText.setText(calculatorObject.currentInput);
+        nextNumber = true;
+    }
+
+        void displayHistory() {
+            String stringList = "";
+            for (String s : historyList)
+                stringList += s + "\n";
+            historyText.setText(stringList);
+        }
+   void clearHistory() {
+       historyList.clear();
+       historyText.setText("");
+
+   }
+
+    void getResult () {
+        int result = calculatorObject.calculate();
+        if (calculatorObject.calculationDone) {
+            calculatorObject.currentInput += result;
+           displayText.setText(calculatorObject.currentInput);
+            if (showHistory) {
+                historyList.add(calculatorObject.currentInput);
+                displayHistory();
+            }
+        }
+    }
     public void onClick(View view) {
-        String value;
+        String values;
         switch (view.getId()){
+            case R.id.cancel:
+               clearResult();
+                break;
+
+            case R.id.enter:
+
+                values = ((Button) view).getText().toString();
+                calculatorObject.push(values);
+                displayText.setText(calculatorObject.currentInput);
+                getResult();
+                nextNumber = true;
+                break;
+
+            case R.id.advance:
+                if (!showHistory) {
+                    ((Button) view).setText("Standard - no History");
+                    view.setBackgroundColor(getColor(R.color.black));
+                    showHistory = true;
+                    displayHistory();
+                }
+
+                    else {
+                        ((Button)view).setText ("ADVANCE - WITH HISTORY");
+                        view.setBackgroundColor (getColor (R.color.mediumpurple));
+                        showHistory = false;
+                        clearHistory();
+                    }
+                    clearResult ();
+                    break;
+
+            default:
+                if (nextNumber) clearResult();
+                values = ((Button)view).getText().toString();
+                calculatorObject.push(values);
+                displayText.setText(calculatorObject.currentInput);
+                nextNumber = false;
+                break;
+
 
         }
 
